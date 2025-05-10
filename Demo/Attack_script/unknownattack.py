e6b840c61bc554ef8d212d172ebdc73c697e6b45
DanilaBaxBax <daniil.kuznetsov2001@mail.ru>
2025-05-10 17:51:35 +0300

    bug fixes

--- Demo/Attack_script/unknownattack.py
+++ Demo/Attack_script/unknownattack.py
@@ -2,10 +2,7 @@ from scapy.all import IP, TCP, send
 import time
 
 def port_scan(target_ip, start_port=1, end_port=200, rate_per_second=100):
-    """
-    Посылает TCP SYN на последовательность портов,
-    чтобы смоделировать порт-сканирование.
-    """
+   
     delay = 1.0 / rate_per_second
     for port in range(start_port, end_port+1):
         pkt = IP(dst=target_ip)/TCP(dport=port, flags='S')
